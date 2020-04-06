b0a58a3fca9686856d09e66e031a1a00493458b5
儒雅的笑哈哈 <1690235898@qq.com>
2020-04-06 17:14:53 +0800

    play

---
+++ js/test.js
@@ -0,0 +1,24 @@
+new Vue({
+    el: "#app",
+
+    data: {
+        isStart: 1,
+        list: [
+            { img: 'img/j1.png', title: '谢谢参与' },
+            { img: 'img/j2.png', title: '美女一个' },
+            { img: 'img/j1.png', title: '宝马一辆' },
+            { img: 'img/j2.png', title: '单车一辆' },
+            { img: 'img/j1.png', title: '鸡蛋一蓝' },
+            { img: 'img/j2.png', title: '500红包' },
+            { img: 'img/j1.png', title: '靓号一个' },
+            { img: 'img/j2.png', title: '鲜花一蓝' }
+        ],
+        showToast: false
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+})
